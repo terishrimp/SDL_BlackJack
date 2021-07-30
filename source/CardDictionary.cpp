72d6003a652a62fa3f6a9e276dd99be656d631fb
terishrimp <72040129+terishrimp@users.noreply.github.com>
2021-07-30 13:30:57 -0400

    Added missing files after updating gitignore

---
+++ source/CardDictionary.cpp
@@ -0,0 +1,12 @@
+#include "Card.h"
+
+static class CardDictionary {
+	void GetCardImage(Card &card) {
+		if (card.getName() == "Ace") {
+			switch (card.getSuit()) {
+			case "spades":
+				return IMG_LoadTexture
+			}
+		}
+	}
+};
